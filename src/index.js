e229042b03917680b9ccf90e65e503bbc29dfccb
Craig Case <case2cn@gmail.com>
2023-05-24 21:48:08 -0600

    Built form in with react framework, adding handlers and useState

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {useState}from 'react';
 import ReactDOM from 'react-dom';
 
+
+
 const Form = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPasswordname] = useState('');
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(username)
+    console.log(password)
+    setUsername('')
+    setPasswordname('')
+    }
+  
+  const handleChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) =>{
+    setPasswordname(event.target.value)
+  }
+
   return (
     <div id='container'>
       <div id='navbar'>
         Form.js
       </div>
-      <form>
-        {/* your form fields here */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor = 'username'>Username:</label>
+        <input type='text' name='username' value={username} onChange={handleChange}/>
+        <label htmlFor='password'>Password:</label>
+        <input type='password' name = 'password' value={password} onChange={handlePasswordChange} />
+        <button type='submit'>Submit</button>
       </form>
     </div>
   )
